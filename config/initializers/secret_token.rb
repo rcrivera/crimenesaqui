55e608743401c4bdcb2ef8423ec1cab8cec6736f
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-03-24 13:57:25 -0400

    Initial commit. Included mongoid gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crimenesaqui::Application.config.secret_key_base = 'e543c036f479bc1d8046d7515c1281beb6b578643a8d771240e35faf3c2261f332f851fed75b89c81617d498bdde4149f514989c06876f45230171e36f0c5ed4'
